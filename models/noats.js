0a2421363313120abe210e08b6e7da5ec6e0aad5
Quackenstien <donaldhawkins422@gmail.com>
2020-06-18 14:24:00 -0700

    added noats back

---
+++ models/noats.js
@@ -0,0 +1,24 @@
+module.exports = function(sequelize, DataTypes) {
+  const Noats = sequelize.define("Noats", {
+    note: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        len: 1,
+      },
+    },
+    recipeId: {
+      type: DataTypes.INTEGER,
+    },
+  });
+
+  Noats.associate = function(models) {
+    Noats.belongsTo(models.Recipe, {
+      foreignKey: {
+        allowNull: false,
+      },
+    });
+  };
+
+  return Noats;
+};
