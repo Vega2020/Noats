c7c4631e5033d4af15fd7d5157f0be063d9267f9
Haven <stackandslash@gmail.com>
2020-06-22 09:24:49 -0700

    Just some comment touch-ups to warm up

--- routes/recipe-api.js
+++ routes/recipe-api.js
@@ -26,7 +26,7 @@ module.exports = function(app) {
     });
   });
 
-  // Get route for retrieving a single post
+  // Get route for retrieving a single recipe and its notes
   app.get("/api/recipe/:id", function(req, res) {
     db.Recipe.findOne({
       where: {
@@ -38,7 +38,7 @@ module.exports = function(app) {
     });
   });
 
-  // POST route for saving a new post
+  // POST route for saving a new recipe name and address
   app.post("/api/recipe", function(req, res) {
     db.Recipe.create({
       recipeName: req.body.recipeName,
@@ -48,7 +48,7 @@ module.exports = function(app) {
     });
   });
 
-  // DELETE route for deleting posts
+  // DELETE route for deleting a recipe
   app.delete("/api/recipe/:id", function(req, res) {
     db.Recipe.destroy({
       where: {
@@ -59,7 +59,7 @@ module.exports = function(app) {
     });
   });
 
-  // PUT route for updating posts
+  // PUT route for updating
   app.put("/api/recipe", function(req, res) {
     db.Recipe.update(req.body, {
       where: {
